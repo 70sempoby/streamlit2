11808d2b1ea7f7a023329231e07efa8bb33743df
shinssam <sempoby@tongjin.hs.kr>
2024-04-25 20:06:41 +0900

    add:app.py input 추가

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.title("Hello 신쌤!!")
+st.title("Hello 신쌤!!👌👌") # 윈도우키 + '.'
+name = st.text_input("이름을 입력해주세요. ")
+
+if name != "" :
+    st.write(f"{name}님! 저의 페이지에 오신 것을 환영합니다!!!!!")
+
+st.balloons()
+
